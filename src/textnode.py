5c768ce927c97cb34d90a9ce2400c46e88fc3518
danl1k3 <like.danyt@gmail.com>
2026-09-11 16:41:09 +0200

    added func textnode to html. Also added tests for it

--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,15 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "normal"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code text"
     LINK = "link"
     IMAGE = "image"
 
-class TextNode:
+class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -24,4 +25,22 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    cont = text_node.text
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, cont)
+        case TextType.BOLD:
+            return LeafNode("b", cont)
+        case TextType.ITALIC:
+            return LeafNode("i", cont)
+        case TextType.CODE:
+            return LeafNode("code", cont)
+        case TextType.LINK:
+            return LeafNode("a", cont, {"href": f"{text_node.url}"})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"})
+        case _:
+            raise Exception("Not supported type of text")
 
